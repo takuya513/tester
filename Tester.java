061109aedd81ea4f2bf854f64e669dc2807df8d7
takuya513 <qwerft513@yahoo.co.jp>
2013-10-28 15:26:18 +0900

    Update Tester.java

--- Tester.java
+++ Tester.java
@@ -1,5 +1,6 @@
 package tester;
 
+import java.io.File;
 import java.util.Arrays;
 import java.util.Scanner;
 import java.util.concurrent.ForkJoinPool;
@@ -12,12 +13,14 @@ import tools.*;
 public class Tester {
 	static Scanner sc;
 	static int outCount = 0;
+	static File file ;
+
 	public static void main(String args[]) throws InterruptedException {
 		sc = new Scanner(System.in);
-
-		sortChecer();
-		//intSorting();
-
+		//file = new File("C:\\Users\\takuya\\Desktop\\chenLec\\chen_zemi\\GraduationStudy\\test.txt");
+		//sortChecer();
+		intSorting();
+		//pinPointCheak();
 		//doubleSorting();
 		//stringSorting();
 	}
@@ -25,7 +28,7 @@ public class Tester {
 
 	public static void stringSorting(){
 
-		int length = 10000;
+		int length = 100000;
 
 		MyString[] stringArray = new MyString[length];
 		String[] data2 = MyArrayUtil.randomStringArray(stringArray.length);
@@ -34,8 +37,8 @@ public class Tester {
 
 		//MyArrayUtil.print(stringArray);
 		Sort<MyString> qs = new QuickSort<MyString>();
-		Sort<MyString> qs6 = new QuickSort6<MyString>();
-		Sort<MyString> qms = new QuickMergeSort<MyString>();//修正
+		Sort<MyString> qs6 = new ParallelQuickSort<MyString>();
+		Sort<MyString> qms = new QuickMergeSort_2<MyString>();//修正
 
 		MyString[] stringArray2 = Arrays.copyOf(stringArray, stringArray.length);
 		MyString[] stringArray3 = Arrays.copyOf(stringArray, stringArray.length);
@@ -44,6 +47,17 @@ public class Tester {
 		long qmsStart = System.currentTimeMillis();
 		qms.sort(stringArray);
 		long qmsEnd = System.currentTimeMillis();
+		System.out.println("KKKKKK");
+		if(!MyArrayUtil.checker(stringArray)){
+
+			System.out.println("OUT");
+
+			outCount++;
+
+			return;
+		}else{
+			System.out.println("OK");
+		}
 
 		long qs6Start = System.currentTimeMillis();
 		qs6.sort(stringArray2);
@@ -66,95 +80,31 @@ public class Tester {
 	}
 
 	public static void sortChecer(){
-		int i = 10;
-		long start = System.currentTimeMillis();
-		//for(int i = 10;i < 1000;i++)
-			intSortingCheck(i);
+		//int i = 24;
 
+
+
+		boolean b = false;
+		long start = System.currentTimeMillis();
+		for(int j = 0;j < 1;j++){
+			for(int i = 99;i < 100;i++)
+				if(!intSortingCheck(i)){
+					b = true;
+					break;
+				}
+			if(b == true)
+				break;
+		}
+
+		//intSortingCheck(i);
 		long stop = System.currentTimeMillis();
 		System.out.println("Finish");
 		System.out.println("outCount : "+outCount);
 		System.out.println("Time : "+(stop-start));
 	}
 
-	private static void doubleSorting() {
+	public static boolean intSortingCheck(int t){
 
-
-//		System.out.println("Input doubleArray length");
-		//int length = sc.nextInt();
-		int length  = 1000000;
-
-
-		MyDouble[] doubleArray = new MyDouble[length];
-		double[] data2 = MyArrayUtil.randomDoubleArray(doubleArray.length);
-		for(int i = 0;i < doubleArray.length;i++)
-			doubleArray[i] = new MyDouble(data2[i]);
-
-		MyDouble[] doubleArray2 = Arrays.copyOf(doubleArray,doubleArray.length);
-		MyDouble[] doubleArray6 = Arrays.copyOf(doubleArray,doubleArray.length);
-		MyDouble[] doubleArray7 = Arrays.copyOf(doubleArray,doubleArray.length);
-		MyDouble[] doubleArray8 = Arrays.copyOf(doubleArray,doubleArray.length);
-		MyDouble[] doubleArray9 = Arrays.copyOf(doubleArray,doubleArray.length);
-		MyDouble[] doubleArray10 = Arrays.copyOf(doubleArray,doubleArray.length);
-		MyDouble[] doubleArray11 = Arrays.copyOf(doubleArray,doubleArray.length);
-
-		Sort<MyDouble> qs = new QuickSort<MyDouble>();
-		Sort<MyDouble> qs2 = new QuickSort2<MyDouble>();
-		Sort<MyDouble> qs6 = new QuickSort6<MyDouble>();
-		Sort<MyDouble> ms = new MergeSort<MyDouble>();
-		Sort<MyDouble> pms = new ParallelMergeSort<MyDouble>();//修正
-		Sort<MyDouble> qms = new QuickMergeSort<MyDouble>();//修正
-
-
-//		MyArrayUtil.print(doubleArray);
-		long qsStart = System.currentTimeMillis();
-		qs.sort(doubleArray);
-		long qsStop = System.currentTimeMillis();
-
-		long qs2Start = System.currentTimeMillis();
-		qs2.sort(doubleArray2);
-		long qs2Stop = System.currentTimeMillis();
-
-
-		long qs6Start = System.currentTimeMillis();
-		qs6.sort(doubleArray6);
-		long qs6Stop = System.currentTimeMillis();
-
-		long msStart = System.currentTimeMillis();
-		ms.sort(doubleArray7);
-		long msStop = System.currentTimeMillis();
-
-
-		long pmsStart = System.currentTimeMillis();
-		pms.sort(doubleArray8);
-		long pmsStop = System.currentTimeMillis();
-
-		long Start = System.currentTimeMillis();
-		Arrays.sort(doubleArray9);
-		long Stop = System.currentTimeMillis();
-
-//		MyArrayUtil.print(doubleArray10);
-		Sort<MyDouble> qs5 = new QuickSort5<MyDouble>();
-		long start9 = System.currentTimeMillis();
-		qs5.sort(doubleArray10);
-		long stop9 = System.currentTimeMillis();
-//		MyArrayUtil.print(doubleArray10);
-
-		long start10 = System.currentTimeMillis();
-		qms.sort(doubleArray11);
-		long stop10 = System.currentTimeMillis();
-
-		System.out.println("Normal Quick Sort time   :"+(qsStop - qsStart));
-		System.out.println("Paralle Quick Sort2 time :"+(qs2Stop - qs2Start));
-		System.out.println("Paralle Quick Sort6 time :"+(qs6Stop - qs6Start));
-		System.out.println("Merge Sort time          :"+(msStop - msStart));
-		System.out.println("ParallelMerge Sort time  :"+(pmsStop - pmsStart)); //修正
-		System.out.println("Arrays Sort time         :"+(Stop - Start)); //修正
-		System.out.println("forkJoin quickSort time  :"+(stop9 - start9)); //修正
-		System.out.println("quickMergeSort time      :"+(stop10 - start10)); //修正
-	}
-
-	public static void intSortingCheck(int t){
 
 		int length = t;
 
@@ -169,49 +119,191 @@ public class Tester {
 		MyInteger[] array10 = Arrays.copyOf(array,array.length);
 		MyInteger[] array11 = Arrays.copyOf(array,array.length);
 		MyInteger[] array12 = Arrays.copyOf(array,array.length);
+		MyInteger[] array13 = Arrays.copyOf(array,array.length);
+		MyInteger[] array14 = Arrays.copyOf(array,array.length);
+		MyInteger[] array9 = Arrays.copyOf(array,array.length);
 
-		Sort<MyInteger> qs6 = new QuickSort6<MyInteger>();
+		Sort<MyInteger> qs6 = new ParallelQuickSort<MyInteger>();
 		Sort<MyInteger> pms = new ParallelMergeSort<MyInteger>();//修正
 		Sort<MyInteger> qms = new QuickMergeSort<MyInteger>();
 		Sort<MyInteger> qms_2 = new QuickMergeSort_2<MyInteger>();
+		Sort<MyInteger> qms3 = new QuickMergeSort3<MyInteger>();
+		Sort<MyInteger> qms4 = new QuickMergeSort4<MyInteger>();
 		Sort<MyInteger> ns = new NewSort<MyInteger>();
+		Sort<MyInteger> ns2 = new NewSort2<MyInteger>();
 
 
 
-		long start11 = System.currentTimeMillis();
-		qms.sort(array10);
-		long stop11 = System.currentTimeMillis();
+		//		long start11 = System.currentTimeMillis();
+		//		qms.sort(array10);
+		//		long stop11 = System.currentTimeMillis();
 
-		long start10 = System.currentTimeMillis();
-		qms_2.sort(array11);
-		long stop10 = System.currentTimeMillis();
+		//		long start10 = System.currentTimeMillis();
+		//		qms_2.sort(array11);
+		//		long stop10 = System.currentTimeMillis();
 
-		MyArrayUtil.print(array12);
+
+		//MyArrayUtil.print(array12);
 		long start12 = System.currentTimeMillis();
-		ns.sort(array12);
+		//ns.sort(array12);
 		long stop12 = System.currentTimeMillis();
-		MyArrayUtil.print(array12);
-		
-		
+		//MyArrayUtil.print(array12);
 		System.out.println("length : "+t);
-		if(!MyArrayUtil.checker(array10)){
-			System.out.println("OUT");
-			outCount++;
-		}
+		long start13 = System.currentTimeMillis();
+		qms4.sort(array13);
+		long stop13 = System.currentTimeMillis();
+//
+//		long start14 = System.currentTimeMillis();
+//		qms_2.sort(array14);
+//		long stop14 = System.currentTimeMillis();
 
-//		System.out.println("Quick Merge Sort_2 time  :"+(stop10 - start10)); //修正
-//		System.out.println("Quick Merge Sort time  :"+(stop11 - start11)); //修正
+		//QuickSort5<MyInteger> qs5 = new QuickSort5<MyInteger>();
+//		qms3 = new QuickMergeSort3<MyInteger>();
+//		long start9 = System.currentTimeMillis();
+//		qms3.sort(array9);
+//		long stop9 = System.currentTimeMillis();
+
+
+
+		//MyArrayUtil.print(array14);
+		if(!MyArrayUtil.checker(array13)){
+
+			System.out.println("OUT");
+			MyArrayUtil.print(array);
+			MyArrayUtil.print(array13);
+
+
+
+			outCount++;
+
+			return false;
+		}else{
+			//System.out.println("OK");
+			//MyArrayUtil.print(array13);
+		}
+		System.out.println();
+		//System.out.println();
+			//System.out.println("start : "+start13+"  stop : "+stop13);
+//		System.out.println("start : "+start14+"  stop : "+stop14);
+//				System.out.println("qms time : "+(stop13 - start13));
+//				System.out.println("qms2 time : "+(stop14 - start14));
+//				System.out.println("qms3 time : "+(stop9 - start9));
+
+		return true;
+		//		System.out.println("Quick Merge Sort_2 time  :"+(stop10 - start10)); //修正
+		//		System.out.println("Quick Merge Sort time  :"+(stop11 - start11)); //修正
 
 
 	}
+	private static void doubleSorting() {
+
+
+		//		System.out.println("Input doubleArray length");
+		//int length = sc.nextInt();
+		int length  = 10000000;
+
+
+		MyDouble[] doubleArray = new MyDouble[length];
+		double[] data2 = MyArrayUtil.randomDoubleArray(doubleArray.length);
+		for(int i = 0;i < doubleArray.length;i++)
+			doubleArray[i] = new MyDouble(data2[i]);
+
+		MyDouble[] doubleArray2 = Arrays.copyOf(doubleArray,doubleArray.length);
+		MyDouble[] doubleArray3 = Arrays.copyOf(doubleArray,doubleArray.length);
+		MyDouble[] doubleArray4 = Arrays.copyOf(doubleArray,doubleArray.length);
+		MyDouble[] doubleArray5 = Arrays.copyOf(doubleArray,doubleArray.length);
+		MyDouble[] doubleArray6 = Arrays.copyOf(doubleArray,doubleArray.length);
+		MyDouble[] doubleArray7 = Arrays.copyOf(doubleArray,doubleArray.length);
+		MyDouble[] doubleArray8 = Arrays.copyOf(doubleArray,doubleArray.length);
+
+		Sort<MyDouble> qs = new QuickSort<MyDouble>();
+		Sort<MyDouble> qs2 = new QuickSort2<MyDouble>();
+		Sort<MyDouble> qs6 = new ParallelQuickSort<MyDouble>();
+		Sort<MyDouble> ms = new MergeSort<MyDouble>();
+		Sort<MyDouble> qms_0 = new QuickMergeSort<MyDouble>();//修正
+		Sort<MyDouble> qms = new QuickMergeSort_2<MyDouble>();//修正
+
+
+		//MyArrayUtil.print(doubleArray);
+		long qsStart = System.currentTimeMillis();
+		qs.sort(doubleArray);
+		long qsStop = System.currentTimeMillis();
+
+		long qs2Start = System.currentTimeMillis();
+		//qs2.sort(doubleArray2);
+		long qs2Stop = System.currentTimeMillis();
+
+
+		long qs6Start = System.currentTimeMillis();
+		//qs6.sort(doubleArray6);
+		long qs6Stop = System.currentTimeMillis();
+
+		long msStart = System.currentTimeMillis();
+		//ms.sort(doubleArray7);
+		long msStop = System.currentTimeMillis();
+
+
+		long qms_0Start = System.currentTimeMillis();
+		qms_0.sort(doubleArray5);
+		long qms_0Stop = System.currentTimeMillis();
+
+		long Start = System.currentTimeMillis();
+		Arrays.sort(doubleArray6);
+		long Stop = System.currentTimeMillis();
+
+		//MyArrayUtil.print(doubleArray10);
+		Sort<MyDouble> qs5 = new QuickSort5<MyDouble>();
+		long start9 = System.currentTimeMillis();
+		qs5.sort(doubleArray7);
+		long stop9 = System.currentTimeMillis();
+		//MyArrayUtil.print(doubleArray10);
+
+		long start10 = System.currentTimeMillis();
+		qms.sort(doubleArray8);
+		long stop10 = System.currentTimeMillis();
+
+		System.out.println("Normal Quick Sort time   :"+(qsStop - qsStart));
+		System.out.println("Paralle Quick Sort2 time :"+(qs2Stop - qs2Start));
+		System.out.println("Paralle Quick Sort6 time :"+(qs6Stop - qs6Start));
+		System.out.println("Merge Sort time          :"+(msStop - msStart));
+		System.out.println("quickMergeSort_0 time  :"+(qms_0Stop - qms_0Start)); //修正
+		System.out.println("Arrays Sort time         :"+(Stop - Start)); //修正
+		System.out.println("forkJoin quickSort time  :"+(stop9 - start9)); //修正
+		System.out.println("quickMergeSort_2 time      :"+(stop10 - start10)); //修正
+	}
+
+
+	public static void pinPointCheak(){
+		MyInteger[] array = new MyInteger[19];
+		int[] data = {5 ,17, 14, 14, 16, 19, 10, 4, 7, 8, 16, 5, 17, 12, 16, 17, 15, 15, 9};
+
+		for(int i = 0;i < array.length;i++)
+			array[i] = new MyInteger(data[i]);
+
+		MyInteger[] afterArray = Arrays.copyOf(array,array.length);
+
+		Sort<MyInteger> ns = new NewSort<MyInteger>();
+
+		ns.sort(afterArray);
+
+		if(!MyArrayUtil.checker(afterArray))
+			System.out.println("OUT");
+		else
+			System.out.println("SAFE");
+
+		MyArrayUtil.print(array);
+		MyArrayUtil.print(afterArray);
+
+	}
+
 
 
 	public static void intSorting(){
 
 		System.out.println("Input arrays length");
 		//int length = sc.nextInt();
-		int length =12;
-//		int length = Integer.MAX_VALUE;
+		int length =100000;  //学校のパソコン用
+		//		int length = Integer.MAX_VALUE;
 		System.out.println("Integer "+Integer.MAX_VALUE );
 
 		MyInteger[] array = new MyInteger[length];
@@ -229,81 +321,109 @@ public class Tester {
 		MyInteger[] array8 = Arrays.copyOf(array,array.length);
 		MyInteger[] array9 = Arrays.copyOf(array,array.length);
 		MyInteger[] array10 = Arrays.copyOf(array,array.length);
+		MyInteger[] array11 = Arrays.copyOf(array,array.length);
+		MyInteger[] array12 = Arrays.copyOf(array,array.length);
+		MyInteger[] array13 = Arrays.copyOf(array,array.length);
 
 		Sort<MyInteger> qs = new QuickSort<MyInteger>();
 		Sort<MyInteger> qs2 = new QuickSort2<MyInteger>();
-		Sort<MyInteger> qs6 = new QuickSort6<MyInteger>();
+		Sort<MyInteger> pqs = new ParallelQuickSort<MyInteger>();
 		Sort<MyInteger> ms = new MergeSort<MyInteger>();
-		Sort<MyInteger> pms = new ParallelMergeSort<MyInteger>();//修正
-		Sort<MyInteger> qms = new QuickMergeSort<MyInteger>();
+		//Sort<MyInteger> pms = new ParallelMergeSort<MyInteger>();//修正
+		Sort<MyInteger> qms = new QuickMergeSort_2<MyInteger>();
+		Sort<MyInteger> ns = new NewSort2<MyInteger>();
+		Sort<MyInteger> qms3 = new QuickMergeSort3<MyInteger>();
+		Sort<MyInteger> qms4 = new QuickMergeSort4<MyInteger>();
 
 
 		//System.out.println("THERA : "+ Runtime.getRuntime().availableProcessors());
 
-		MyArrayUtil.print(array);
+		//		MyArrayUtil.print(array);
 		System.out.println("-------QuickSort------");
 		long start = System.currentTimeMillis();
 		qs.sort(array);
 		long stop = System.currentTimeMillis();
-//		MyArrayUtil.print(array);
+		//		MyArrayUtil.print(array);
 
 		System.out.println("-------QuickSort2------");
 		long start2 = System.currentTimeMillis();
 		qs2.sort(array2);
 		long stop2 = System.currentTimeMillis();
-//		MyArrayUtil.print(array2);
+		//		MyArrayUtil.print(array2);
 
 
 		System.out.println("-------ArraySort------");
 		long start4 = System.currentTimeMillis();
 		Arrays.sort(array4);
 		long stop4 = System.currentTimeMillis();
-//		MyArrayUtil.print(array4);
+		//		MyArrayUtil.print(array4);
 
 
-		System.out.println("-------QuickSort6------");
+		System.out.println("------ParallelQuickSort------");
 		long start6 = System.currentTimeMillis();
-		qs6.sort(array6);
+		//pqs.sort(array6);
 		long stop6 = System.currentTimeMillis();
-//		MyArrayUtil.print(array66);
+		//		MyArrayUtil.print(array66);
 
 		System.out.println("-------MergeSort------");
 		long start7 = System.currentTimeMillis();
-		ms.sort(array7);
+		//ms.sort(array7);
 		long stop7 = System.currentTimeMillis();
-//		MyArrayUtil.print(array7);
+		//		MyArrayUtil.print(array7);
 
-		System.out.println("-------ParallelMergeSort------");
+		System.out.println("-------NewSort------");
 		long start8 = System.currentTimeMillis();
-		pms.sort(array8);
+		//ns.sort(array8);
 		long stop8 = System.currentTimeMillis();
 
 
-//		MyArrayUtil.print(array8);
+		//		MyArrayUtil.print(array8);
 		QuickSort5<MyInteger> qs5 = new QuickSort5<MyInteger>();
 		long start9 = System.currentTimeMillis();
 		qs5.sort(array9);
 		long stop9 = System.currentTimeMillis();
 		//MyArrayUtil.print(array9);
 
+		//		MyArrayUtil.print(array8);
+		ParallelMergeSort<MyInteger> pms = new ParallelMergeSort<MyInteger>();
+		long start11 = System.currentTimeMillis();
+		//pms.sort(array11);
+		long stop11 = System.currentTimeMillis();
+		//MyArrayUtil.print(array9);
+
 		System.out.println("-------QuickMergeSort------");
 		long start10 = System.currentTimeMillis();
 		qms.sort(array10);
 		long stop10 = System.currentTimeMillis();
-		MyArrayUtil.print(array10);
-		if(!MyArrayUtil.checker(array10)){
+
+		System.out.println("-------QuickMergeSort3------");
+		long start12 = System.currentTimeMillis();
+		qms3.sort(array12);
+		long stop12 = System.currentTimeMillis();
+
+
+		System.out.println("-------QuickMergeSort4------");
+		long start13 = System.currentTimeMillis();
+		qms4.sort(array13);
+		long stop13 = System.currentTimeMillis();
+
+		//		MyArrayUtil.print(array10);
+		if(!MyArrayUtil.checker(array12)){
 			System.out.println("False");
 		}
 
 
 		System.out.println("Normal Quick Sort time   :"+(stop - start));
 		System.out.println("Parallel Quick Sort time :"+(stop2 - start2));
-		System.out.println("Parallel Quick Sort6 time:"+(stop6 - start6)); //修正
+		System.out.println("ParallelQuickSort time   :"+(stop6 - start6)); //修正
 		System.out.println("ArraysSort time          :"+(stop4 - start4)); //修正
 		System.out.println("Merge Sort time          :"+(stop7 - start7)); //修正
-		System.out.println("ParallelMerge Sort time  :"+(stop8 - start8)); //修正
+		System.out.println("New Sort time            :"+(stop8 - start8)); //修正
 		System.out.println("forkJoin quickSort time  :"+(stop9 - start9)); //修正
-		System.out.println("Quick Merge Sort time  :"+(stop10 - start10)); //修正
+		System.out.println("Quick Merge Sort time    :"+(stop10 - start10)); //修正
+		System.out.println("Prallel Merge Sort time  :"+(stop11 - start11)); //修正
+		System.out.println("Prallel Merge Sort3 time :"+(stop12 - start12)); //修正
+		System.out.println("Prallel Merge Sort4 time :"+(stop13 - start13)); //修正
 
 	}
 
